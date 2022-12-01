a6df6c35fa139fac0a4fa47c7e62fc8af5c64253
maybe-apex <utkarshomer6972@gmail.com>
2022-12-01 16:30:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+//var goog = require('goog').goog;
+//const OmidSessionClient = goog.module('omid.sessionClient');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+	//let sessionClient;
+	//try {
+	//	sessionClient = OmidSessionClient['default'];
+	//} catch (e) { /* Can't use OM SDK :( */ return; }
+	//if (!sessionClient) { /* Can't use OM SDK :( */ return; }
+	//const AdSession = sessionClient.AdSession;
+	//const Partner = sessionClient.Partner;
+	//const Context = sessionClient.Context;
+	//const VerificationScriptResource = sessionClient.VerificationScriptResource;
+	//const AdEvents = sessionClient.AdEvents;
+	//const MediaEvents = sessionClient.MediaEvents;
+
+	return (
+		<div className="App">
+			<h1>kdfjns</h1>
+			<video width="300" class="omid-element" id="player" controls playsinline autoplay muted>
+				<source src="https://omsdk-demo-files.s3.us-west-2.amazonaws.com/ra_1.3/IABTL_VAST_Intro_30s.mp4" type="video/mp4" />
+			</video>
+		</div>
+	);
 }
 
 export default App;
